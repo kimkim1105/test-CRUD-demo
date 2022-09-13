2d0389421e4df9d3b9e2dbf1735965ae98fe8e31
unknown <huenk105@gmail.com>
2022-09-13 16:05:33 +0700

    caa

--- src/main/java/com/example/demospring/DemospringApplication.java
+++ src/main/java/com/example/demospring/DemospringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemospringApplication {
-
+//aigwuiyoigqcuoq
     public static void main(String[] args) {
         SpringApplication.run(DemospringApplication.class, args);
     }
